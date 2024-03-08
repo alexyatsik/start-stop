6d790fe0d832efd4f287526cc93a153aec9887fe
Oleksii Yatsenko <alexeii@bmby.com>
2024-03-08 09:12:54 +0200

    save state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import {useRef, useState} from "react";
+
 export default function Player() {
+  const playerNameRef = useRef();
+
+  const [playerName, setPlayerName] = useState(null);
+
+  function buttonClickHandler() {
+    setPlayerName(playerNameRef.current.value);
+  }
+
+  const playerNameTitle = playerName ?? 'unknown entity';
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome, {playerNameTitle}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameRef} type="text" />
+        <button onClick={buttonClickHandler}>Set Name</button>
       </p>
     </section>
   );
